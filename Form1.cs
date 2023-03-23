c6a8e325dc22f4e7d7077133e369bcbb93186f79
Adriana <pereira.adri@gmail.com>
2023-03-23 17:35:49 -0300

    projeto parte 3

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace EditorTXT
 {
     public partial class Form1 : Form
@@ -27,11 +29,52 @@ namespace EditorTXT
 
         }
         private void mArquivoSalvar_Click(object sender, EventArgs e)
-        {
+        {            
+            if (File.Exists(Gerenciador.FilePath))
+            {
+                SalvarArquivo(Gerenciador.FilePath);
+            }
+            else
+            {
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.Title = "Salvar...";
+                dialog.Filter = "rich text file|*.rtf|texto|*.txt|todos|*.*";
+                dialog.CheckFileExists = false;
+                dialog.CheckPathExists = true;
 
+                var result = dialog.ShowDialog();
+                if (result != DialogResult.Cancel && result != DialogResult.Abort)
+                {
+                    SalvarArquivo(dialog.FileName);
+                }
+            }
         }
         private void mArquivoSalvarComo_Click(object sender, EventArgs e)
         {
+
+        }
+        private void SalvarArquivo(string path)
+        {
+            // objeto responsável por escrever o arquivo
+            StreamWriter writer = null;
+            try
+            {
+                writer = new StreamWriter(path, false);
+                writer.Write(txtConteudo.Text);
+
+                FileInfo file = new FileInfo(path);
+                Gerenciador.FolderPath = file.DirectoryName + "\\";
+                Gerenciador.FileName = file.Name.Remove(file.Name.LastIndexOf(".")); // remove a extensão do arquivo
+                Gerenciador.FileExt = file.Extension;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao salvar: \n" + ex.Message);                
+            }
+            finally
+            {
+                writer.Close();
+            }
 
         }
         private void mArquivoSair_Click(object sender, EventArgs e)
